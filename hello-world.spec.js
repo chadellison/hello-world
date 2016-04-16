39f6b7a7fb99cae1bc901af620b5ac3825d03c34
Chad Ellison <chad.ellison0123@gmail.com>
2016-04-16 17:01:04 -0600

    Hello World

--- hello-world.spec.js
+++ hello-world.spec.js
@@ -7,11 +7,35 @@ describe('Hello World', function() {
     expect(helloWorld.hello('')).toEqual('Hello, World!');
   });
 
-  xit('says hello to bob', function() {
+  it('says hello to bob', function() {
     expect(helloWorld.hello('Bob')).toEqual('Hello, Bob!');
   });
 
-  xit('says hello to sally', function() {
+  it('says hello to sally', function() {
     expect(helloWorld.hello('Sally')).toEqual('Hello, Sally!');
   });
 });
+
+// template strings === string interpolation
+  // return `Hello, ${input || "World"}!`
+
+// prototypical inheritance (vs classical inheritance in ruby)
+
+// { state: state } -----------------> { state }
+
+// todo = { text: "what" }
+// store.dispatch(todo, 'ADD_TODO')
+  // reducers -> pure functions (entirely functional), can't modify state
+    // state, action is passed in
+    // Object.assign({}, state, { calculation })
+      // time travel --> cycle back and forth through states
+        // (redux-undo) state.past / state.present / state.future
+
+  // change the state of the appl
+  // that state trickles down and updates your components
+
+// this.props.onAddTodo(todo)
+
+// jasmine
+// mocha/chai/expect/assert
+  // jasmine-node (short cut)
